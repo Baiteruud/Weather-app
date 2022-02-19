9154ecd6754402425ada07173db93c4191d6ff16
Hashhuu-y1 <Hashhuu-y1@iMac-10s-iMac.local>
2022-02-19 12:46:21 +0800

    hotiin ner suggest hiidg bolsn

--- src/enginer.js
+++ src/enginer.js
@@ -1,6 +1,44 @@
+const udur = document.querySelector('.date');
+const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+const now = new Date();
+const date = monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+udur.textContent = date;
 const api={
     apiKey: "ab78e8a71989db5a748fbf7ad09b956c",
     baseUrl: 'https://api.openweathermap.org/data/2.5/weather?q=',
+}
+
+let autocomplete;
+function initAutocomplete(){
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+            types: ['geocode'],
+            fields: ['name']
+        }
+    );
+    autocomplete.addListener('place_changed', onPlaceChanged)
+}
+function onPlaceChanged(){
+    var place = autocomplete.getPlace();
+    if(!place.geometry){
+        document.getElementById('autocomplete').placeholder = "Not you";
+    }else{
+        document.getElementById('autocomplete').innerHTML = place.name;
+    }
 }
 
 const input = document.querySelector('.searchbar')
@@ -17,7 +55,16 @@ function sendRequest(cityName){
         return weather.json()
     }).then(data => displayData(data));
 }
-
 function displayData(weatherData){
     console.log(weatherData)
+    const cityName = document.querySelector('.city');
+    const temperature = document.querySelector('.temp');
+    const status = document.querySelector('.status');
+    status.textContent = weatherData.weather[0].main;
+    cityName.textContent = weatherData.name + ", " + weatherData.sys.country;
+    temperature.textContent = Math.floor(weatherData.main.temp - 273) + "°";
+    // switch(weatherData.weather[0].main){
+    //     case "Rain":
+    
+    // }
 }
