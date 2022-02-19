31694c893dd1f076b3276a91e8ad7efbb69395a0
buyantogtoh-y1 <buyantogtoh-y1@iMac-17s-iMac.local>
2022-02-19 09:46:25 +0800

    issue9

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
+const udur = document.querySelector("#date");
+const monthNames=[
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+    
+
+]
+const now = new Date();
+const month = monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear(); 
+udur.textContent = month;
+
 const api = {
     apiKey:"375b3a685e5d0a1ac3ea7363f8f7039b",
     baseUrl:"https://api.openweathermap.org/data/2.5/weather?q=",
@@ -17,6 +38,8 @@ function sendRequest(cityName){
 }
 
 function redirectData(data) {
-    console.log(data.main.temp);
-
+    console.log(data.main);
 }
+
+    
+    
