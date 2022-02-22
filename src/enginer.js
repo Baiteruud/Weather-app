b304acb8a4bdd18382c10756484279f5eb9cb0a7
altanochirH1 <altanochir@iMac2021-07s-iMac.local>
2022-02-22 15:52:30 +0800

    commited

--- src/enginer.js
+++ src/enginer.js
@@ -1,22 +1,78 @@
- const api ={
- apiKey: "7e3cbf881415c036a41c7387f0191219",
- baseuUrl: 'https://api.openweathermap.org/data/2.5/weather?q=',
+const udue = document.querySelector('.date');
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const now = new Date();
+const date = monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear();
+udue.textContent = date;
+
+
+const api = {
+  apiKey: "7e3cbf881415c036a41c7387f0191219",
+  baseUrl: "https://api.openweathermap.org/data/2.5/weather?q=",
+};
+
+let  autocomplete;
+const input = document.querySelector(".searchBar");
+
+function initAutocomplete() {
+ autocomplete = new google.maps.places.Autocomplete(
+  document.getElementById('autocomplete'),
+   {
+     types:['geocode'],
+     fields:['name']
+    }  
+  );
+  console.log(autocomplete)
+ autocomplete.addListener('place_changed', onPlaceChanged)
 }
-const input = document.querySelector('.searchBar')
-input.addEventListener('keypress', listener);
+
+function onPlaceChanged(){
+  var place = autocomplete.getPlace();
+  if(!place.geometry){
+    document.getElementById('autocomplete').innerHTML = place.name;
+  }else{
+    document.getElementById('autocomplete').innerHTML = place.name;
+  }
+}
+/* 
+const input = document.querySelector(".searchBar"); */
+input.addEventListener("keypress", listener);
 
 function listener(event) {
- if(event.keyCode == 13){
-   sendRequest(input.value);
- } 
+  if (event.keyCode == 13) {
+    sendRequest(input.value);
+  }
 }
 
 function sendRequest(cityName) {
- fetch(`${api.baseuUrl}${cityName}&appid=${api.apiKey}`).then(weather =>{
-  return weather.json()
- }).then(diplayData);
+  fetch(`${api.baseUrl}${cityName}&appid=${api.apiKey}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayData);
 }
 
-function diplayData(weatherData){
-  console.log(weatherData)
+function displayData(tsagUur) {
+  console.log(tsagUur);
+  const cityName = document.querySelector('.city');
+  const temperature = document.querySelector('.temp');
+  const status = document.querySelector('.status');
+//   displaying cityName
+  cityName.textContent = tsagUur.name + ", " + tsagUur.sys.country;
+//   displaying converted temperature
+  temperature.textContent = Math.floor(tsagUur.main.temp - 273)+ "°";
+// displaying status
+  status.textContent = tsagUur.weather[0].main;
 }
